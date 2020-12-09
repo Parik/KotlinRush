551ce04760364e31a442a17d46e56f5c497b0341
Igor Ponomarev <56533018+Parik@users.noreply.github.com>
2020-12-09 13:27:21 +0300

    1

--- src/main/level02/task01/l02t01.kt
+++ src/main/level02/task01/l02t01.kt
@@ -5,19 +5,49 @@ package main.level02.task01
 Подсказка:
 Нужно написать тело существующей функции min и исправить возвращаемое значение.
 */
-
-val a = 12
-val b = 10
-
 fun min2(a:Int, b:Int){
-    if (a<b){
+    if (a < b && a != b){
         println("Минимальное число $a")
-    } else {
+    } else if (b<a && a != b){
         println("Минимальное число $b")
+    } else {
+        println("Числа равны")
     }
 }
 
+fun min(a: Int, b:Int): Int {
+    var m1 = 0
+    if (a<b){
+       m1 = a
+   } else {
+       m1 = b
+   }
+    return m1
+}
 
 fun main(){
-    min2(a, b)
-}
+    print("Введите первое число: ")
+    var ina = readLine()
+    var a = ina!!.toInt()
+    print("Введите второе число: ")
+    var inb = readLine()
+    var b = inb!!.toInt()
+    print("Введите третье число: ")
+    var inc = readLine()
+    var c = inc!!.toInt()
+    print("Введите четвертое число: ")
+    var ind = readLine()
+    var d = inb!!.toInt()
+
+    var m2 = min(a, b)
+    var m3 = min(c, d)
+
+    var out:Int
+    if (m2 < m3){
+        out = m2
+    } else {
+        out = m3
+    }
+
+    println("Наименьшее число $out")
+  }
