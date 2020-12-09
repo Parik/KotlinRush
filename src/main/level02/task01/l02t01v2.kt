212289c4b363f31c6bfe031939febf88285d3ca8
Igor Ponomarev <56533018+Parik@users.noreply.github.com>
2020-12-09 15:59:46 +0300

    1

---
+++ src/main/level02/task01/l02t01v2.kt
@@ -0,0 +1,20 @@
+package main.level02.task01
+//
+//fun math(a)
+//
+//fun main(){
+//    print("Введите первое число: ")
+//    var ina = readLine()
+//    var a = ina!!.toInt()
+//    print("Введите второе число: ")
+//    var inb = readLine()
+//    var b = inb!!.toInt()
+//    print("Введите третье число: ")
+//    var inc = readLine()
+//    var c = inc!!.toInt()
+//    print("Введите четвертое число: ")
+//    var ind = readLine()
+//    var d = ind!!.toInt()
+//
+//        println("Наименьшее число $out")
+//}
